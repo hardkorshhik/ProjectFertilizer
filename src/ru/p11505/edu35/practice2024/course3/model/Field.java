8dddac85ba7c904c86a09c131fdad2172aec9467
hardkorshhik <hardkorshhikmine@gmail.com>
2024-11-29 02:20:15 +0300

    123

---
+++ src/ru/p11505/edu35/practice2024/course3/model/Field.java
@@ -0,0 +1,42 @@
+package ru.p11505.edu35.practice2024.course3.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Field {
+    private final double area;
+    private final Map<Fertilizer, Double> consumptionPerSquareMeter = new HashMap<>();
+
+    public Field(double area) {
+        this.area = area;
+    }
+
+    public void addFertilizerConsumption(Fertilizer fertilizer, double consumption) {
+        consumptionPerSquareMeter.put(fertilizer, consumption);
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public boolean containsFertilizer(Fertilizer fertilizer) {
+        return consumptionPerSquareMeter.containsKey(fertilizer);
+    }
+
+
+    public FertilizerStats getTotal(Fertilizer fertilizer) {
+        // Проверяем, зарегистрировано ли удобрение для поля
+        if (!consumptionPerSquareMeter.containsKey(fertilizer)) {
+            throw new IllegalArgumentException("Удобрение не зарегистрировано для данного поля.");
+        }
+
+        // Общая потребность в удобрении
+        double totalNeed = area * consumptionPerSquareMeter.get(fertilizer);
+
+        // Актуальное количество удобрения, добавленного через FertilizerPlanner
+        double currentAmount = Double.min(fertilizer.getQuantity(), totalNeed);
+
+        return new FertilizerStats(totalNeed, currentAmount);
+    }
+
+}
